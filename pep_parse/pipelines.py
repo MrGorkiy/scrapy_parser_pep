ef16d22337463101ae4270d5c9f917f0ebc3fc65
MrGorkiy <mbrysin@bk.ru>
2022-11-22 19:35:15 +0300

    FIX Исправлены замечания ревьюера
    Удаляет файл с константами из за тестов

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,11 +1,24 @@
 import csv
+import datetime as dt
+from pathlib import Path
 
-from pep_parse.constants import FILE_DIR, FIELDS_NAME
+# При попытке вынести все константы в отдельный файл, заваливаются тесты,
+# но функциональность полностью сохраняется
+BASE_DIR = Path(__file__).parent.parent
+
+FIELDS_NAME = ('Статус', 'Количество')
+DIR_OUTPUT = 'results'
+DT_FORMAT = '%Y-%m-%dT%H-%M-%S'
+FILE_NAME = 'status_summary_{time}.csv'
+TIME_NOW = dt.datetime.now().strftime(DT_FORMAT)
+
 
 class PepParsePipeline:
     def open_spider(self, spider):
-        """Создание словаря для подсчета."""
+        """Формирование пути до директории results."""
         self.results = {}
+        self.result_dir = BASE_DIR / DIR_OUTPUT
+        self.result_dir.mkdir(exist_ok=True)
 
     def process_item(self, item, spider):
         """Подсчет количества статусов."""
@@ -17,7 +30,11 @@ class PepParsePipeline:
         return item
 
     def close_spider(self, spider):
-        with open(FILE_DIR, mode='w', encoding='utf-8') as f:
+        """Запись данных в файл."""
+
+        file_dir = self.result_dir / FILE_NAME.format(
+            time=TIME_NOW)
+        with open(file_dir, mode='w', encoding='utf-8') as f:
             writer = csv.writer(f, dialect='unix')
             writer.writerow((FIELDS_NAME))
             for key, val in self.results.items():
